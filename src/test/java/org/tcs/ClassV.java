0b2735ec765a1a5351f329168e4643e11365d6f7
Rajaa C <rajaachandramohan@gmail.com>
2024-04-15 22:30:59 +0530

    init commit

--- src/test/java/org/tcs/ClassV.java
+++ src/test/java/org/tcs/ClassV.java
@@ -1,20 +1,21 @@
 package org.tcs;
 
 public class ClassV {
+	
+	private void m1() {
+		System.out.println("Hello");
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+	}
+	
 	public static void main(String[] args) {
 		
 		SampleA a = new SampleA();
 		a.setAge(23);
 		a.setName("ram");
-		
-	
+			
 	}
-	
-	
-	
-
-	
-	
-	
 
 }
